deee80676a76a9568b80a9358e9d6ca24ae203d7
chathuranga <chaturanga@itone.biz>
2026-09-16 13:46:12 +0530

    aaa

--- pos-api/src/config/tenants.js
+++ pos-api/src/config/tenants.js
@@ -1,6 +1,6 @@
 // Maps each subdomain to its MySQL database
 module.exports = {
-  'shakya.lumac.cc': {
+  'shakya-dist.lumac.cc': {
     database: 'sakya_dist',
     username: 'pos_user',
     password: 'Pos@2026Strong',
